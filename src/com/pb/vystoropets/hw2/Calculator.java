7d7328a3f0bdd0f59c66b64a4c1ce2b50f030059
Ilya Vystoropets <ilja.vystoropets@privatbank.ua>
2021-10-15 19:05:07 +0300

    init commit

---
+++ src/com/pb/vystoropets/hw2/Calculator.java
@@ -0,0 +1,38 @@
+package com.pb.vystoropets.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int operand1;
+        int operand2;
+        int result = 0;
+        char operation;
+        System.out.println("Ввидите число 1:");
+        operand1 = scan.nextInt();
+        System.out.println("Ввидите операцию:");
+        operation = scan.next().charAt(0);
+        System.out.println("Ввидите число 2:");
+        operand2 = scan.nextInt();
+
+        switch (operation) {
+            case '+': result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*': result = operand1 * operand2;
+                break;
+            case '/':if (operand2 == 0) {
+                System.out.println("делить на 0 запрещено");
+            } else {
+                result = operand1 / operand2;
+            }
+                break;
+            default:  System.out.println("Упс! Что-то пошло не так");
+                return;
+        }
+        System.out.println("Итог" + result);
+    }
+}
